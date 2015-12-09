07d0554023c4770487707031755d85ed286f17e0
Patrik Bergman <patrik.bergman@transmode.com>
2015-12-09 14:49:05 +0100

    Added simplelogger.properties Continued investigation on threads.

--- src/main/java/se/patrikbergman/java/concurrency/runnable/BandApplication.java
+++ src/main/java/se/patrikbergman/java/concurrency/runnable/BandApplication.java
@@ -1,16 +1,24 @@
 package se.patrikbergman.java.concurrency.runnable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class BandApplication {
+    final static Logger log = LoggerFactory.getLogger(BandMember.class.getSimpleName());
 
     public static void main(String args[]) {
-        BandMember john = new BandMember("John", 5000);
-        BandMember paul = new BandMember("Paul", 100);
+        BandMember john = new BandMember("John");
+        BandMember paul = new BandMember("Paul");
 
         john.setFellowBandMember(paul);
         paul.setFellowBandMember(john);
 
-        john.start();
-        paul.start();
+        new Thread(() -> {
+            john.addressFellowBandMember();
+        }).start();
 
+        new Thread(() -> {
+            paul.addressFellowBandMember();
+        }).start();
     }
 }
--- src/main/java/se/patrikbergman/java/concurrency/runnable/BandMember.java
+++ src/main/java/se/patrikbergman/java/concurrency/runnable/BandMember.java
@@ -3,54 +3,35 @@ package se.patrikbergman.java.concurrency.runnable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class BandMember extends Thread {
+class BandMember {
     final Logger log = LoggerFactory.getLogger(BandMember.class.getSimpleName());
     private final String name;
-    private final long initialSleepTime;
     private BandMember fellowBandMember;
 
-    BandMember(String name, long initialSleepTime) {
+    BandMember(String name) {
         this.name = name;
-        this.initialSleepTime = initialSleepTime;
-    }
-
-    public void run() {
-        doWait(initialSleepTime);
-        addressFellowBandMember();
     }
 
     void addressFellowBandMember() {
-        log.info("{} is addressing fellow band member {}",
-                this.getFirstnameAndThreadName(),
-                fellowBandMember.getFirstnameAndThreadName());
-        String response = fellowBandMember.address("Hi from " + this.getFirstnameAndThreadName());
-        log.info("{} recieved response '{}''", this.getFirstnameAndThreadName(), response);
+        log.info("{} is addressing {}", name, fellowBandMember.getName());
+        final String response = fellowBandMember.address("Hi from " + this.getName());
+        log.info("{} recieved response '{}''", name, response);
     }
 
     /**
      * Invoked by other BandMembers
      */
     String address(String message) {
-        if(this.getState().equals(State.TIMED_WAITING)) {
-            log.info("{} received message while waiting...", this.getFirstnameAndThreadName());
-        }
-        log.info("{} received message '{}'", this.getFirstnameAndThreadName(), message);
-        return "Yabadabadoo! (from " + this.getFirstnameAndThreadName() + ")";
+        log.info("{} received message '{}'", this.getName(), message);
+        return "Yabadabadoo! (from " + this.getName() + ")";
     }
 
-    String getFirstnameAndThreadName()  {
-        return name + " [" + getName() + "]";
+    String getName()  {
+        return name;
     }
 
     void setFellowBandMember(BandMember fellowBandMember) {
         this.fellowBandMember = fellowBandMember;
     }
 
-    private void doWait(long waitTime) {
-        try {
-            sleep(waitTime);
-        } catch (InterruptedException e) {
-            log.error("{} was interrupted during wait", getFirstnameAndThreadName());
-        }
-    }
 }
